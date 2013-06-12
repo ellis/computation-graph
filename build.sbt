c480b9c1783168931411116f578c9b2459fd23f8
Ellis Whitehead <ellis@ellisw.net>
2013-06-12 15:32:59 +0200

    .

--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ libraryDependencies ++= Seq(
 	"org.scalaz" %% "scalaz-core" % "7.0.0",
 	"org.clapper" %% "grizzled-slf4j" % "1.0.1",
 	"ch.qos.logback" % "logback-classic" % "1.0.7",
-	"org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+	"org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+	"com.assembla.scala-incubator" % "graph-core_2.10" % "1.6.1"
 )
--- src/main/scala/ch/ethz/computationgraph/ComputationGraph.scala
+++ src/main/scala/ch/ethz/computationgraph/ComputationGraph.scala
@@ -1,10 +1,34 @@
 package ch.ethz.computationgraph
 
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
 import scalaz._
 import Scalaz._
 import grizzled.slf4j.Logger
 import scala.collection.mutable.ArrayBuffer
+import scalax.collection.Graph, scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
+
+trait Command
+
+case class Command_AddEntity
+case class Command_AddCall(call: Call, time: List[Int]) extends Command
+
+class ComputationGraphBuilder {
+	val db = new EntityBase
+	val selectorToValue_m = new HashMap[Selector, Object]
+	val call_l = new ArrayBuffer[Call]
+	val callChildren_m = new HashMap[Call, List[Call]]
+	val selectorToCall_l = new HashSet[(Selector, Call)]
+	
+	/**
+	 * When a call is added, its selectors are added to the selector/call graph
+	 */
+	def addCall(call: Call, time: List[Int]) {
+		call_l += call
+		for (selector <- call.args)
+			selectorToCall_l += (selector -> call)
+	}
+}
 
 class ComputationGraph {
 	private val logger = Logger[this.type]
