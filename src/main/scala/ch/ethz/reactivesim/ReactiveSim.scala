4d13a466001bc4eac6e584c18063a1507c6c9caf
ellis <ellis@ellisw.net>
2013-07-02 09:00:47 +0200

    create ReactiveSim class

---
+++ src/main/scala/ch/ethz/reactivesim/ReactiveSim.scala
@@ -0,0 +1,15 @@
+package ch.ethz.reactivesim
+
+class ReactiveSim {
+	private var cg = ComputationGraph()
+	
+	def graph = cg
+	
+	def addCall(call: Call) {
+		cg = cg.addCall(call)
+	}
+	
+	def setInitialState(id: String, entity: Object) {
+		cg = cg.setInitialState(id, entity)
+	}
+}
